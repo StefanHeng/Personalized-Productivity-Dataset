dff45efa6bb8628464dec43f8759fe21e1651c2b
Stefan Heng <stefan.hg@outlook.com>
2022-08-02 15:40:44 +0800

    Update: fetch data call done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Personalized-Productivity-Dataset
 Personalized Text Classification dataset with transient labels inspired by Myca productivity tool
+
+Python version `3.9.12`.
+
--- crawl-myca.py
+++ crawl-myca.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 
 import pandas as pd
@@ -16,19 +17,28 @@ if __name__ == '__main__':
         df = pd.read_csv(path)
         auth = df.iloc[0, :].to_dict()
         body = dict(email=auth['username'], password=auth['password'])
-        headers = {'Content-Type': 'application/json'}  # adding this makes parse error?
+        headers = {'Content-Type': 'application/json'}  # TODO: adding this makes parse error in response?
 
         r = requests.post(
-            url,
-            data=body,
+            url=url, data=body,
             # headers=headers
         )
         mic(r)
-        mic(r.text)
+        res = json.loads(r.text)
+        mic(res)
+        fnm = f'{now(for_path=True)}-admin-token.json'
+        path_out = os.path.join('auth', 'myca', fnm)
+        with open(path_out, 'w') as f:
+            json.dump(res, f, indent=4)
     # create_token()
 
     def fetch_data_by_day():
         url = f'{base_url}/js/walker_run'
+
+        token_fnm = '2022-08-02_15-36-01-admin-token.json'
+        token_path = os.path.join('auth', 'myca', token_fnm)
+        with open(token_path) as f:
+            token = json.load(f)['token']
 
         path = os.path.join('auth', 'myca', 'user-ids.txt')
         with open(path, 'r') as f:
@@ -37,17 +47,19 @@ if __name__ == '__main__':
         user_id = user_ids[0]
 
         body = dict(
-            name="get_latest_day",
+            name='get_latest_day',
             nd=user_id,
             ctx=dict(
-                before_date="2022-08-01",
+                before_date='2022-08-01',
                 show_report=1  # this must be 1 or otherwise the response will be empty
             )
         )
         r = requests.post(
-            url,
-            data=body
+            url=url, data=body,
+            headers=dict(Authorization=f'token {token}')
         )
         mic(r)
-        mic(r.text)
+        res = json.loads(r.text)
+        mic(res)
+        print(res['stack_trace'])
     fetch_data_by_day()
